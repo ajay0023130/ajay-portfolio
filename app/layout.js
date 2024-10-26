e06054fb9286748938e2f328ccb25db6023a5ef4
ajay babu <ajaybabu0046@gmail.com>
2024-10-27 04:02:14 +0530

    complete home page

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/ui/StairTransition";
+
 const jetBrains_mono = JetBrains_Mono({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
