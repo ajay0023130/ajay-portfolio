937e4e67073e0ed034110ccbfdb566645969cc4c
ajay babu <ajaybabu0046@gmail.com>
2024-11-06 23:59:18 +0530

    updated code

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,12 +1,68 @@
-import React from 'react'
+"use client";
 
-const services = () => {
+import { BsArrowDownRight } from "react-icons/bs";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const servicesList = [
+  {
+    num: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae placeat illum deleniti id!",
+    href: "",
+  },
+  {
+    num: "02",
+    title: "API Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae placeat illum deleniti id!",
+    href: "",
+  },
+  {
+    num: "03",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae placeat illum deleniti id!",
+    href: "",
+  },
+  {
+    num: "04",
+    title: "Devops Services",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae placeat illum deleniti id!",
+    href: "",
+  },
+];
+
+const Services = () => {
   return (
-    <div>
-        services
-      
-    </div>
-  )
-}
+    <section className="min-h-[80vh] flex flex-col justify-center py-16 md:py-12">
+      <div className="container mx-auto">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeIn" } }}
+          className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
+        >
+          {servicesList.map((service, index) => (
+            <div key={index} className="flex flex-col justify-center gap-4 group">
+              <div className="w-full flex justify-between items-center ">
+                <span className="text-4xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{service.num}</span>
+                <Link href={service.href} className="w-[45px] h-[45px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                  <BsArrowDownRight className="text-primary text-3xl   " />
+                </Link>
+              </div>
+              <h2 className="text-[38px] font-bold leading-none text-white  group-hover:text-accent transition-all duration-500">{service.title}</h2>
+              <p className="text-white/60">{service.description}</p>
+              <div className="border-b border-white/20 w-full " >
 
-export default services
+              </div>
+            </div>
+          ))}
+        </motion.div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
