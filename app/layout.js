0469f3eb536269a4dff7b7a452167f5f967ffa06
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2024-11-07 11:22:00 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,13 @@
-import { Playfair_Display } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/ui/StairTransition";
 
-const playfair_display = Playfair_Display({
+const jetBrains_mono = JetBrains_Mono({
   subsets: ["latin"],
-  weight: ["400", "500", "600", "700", "800", "900"],
-  variable: "--font-playfairdisplay", // CSS variable for custom font
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+  variable: "--font-jetbainsmono", // CSS variable for custom font
 });
 
 export const metadata = {
@@ -18,12 +18,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${playfair_display.variable} antialiased`}>
+      <body className={`${jetBrains_mono.variable} antialiased`}>
         <Header/>
         <StairTransition/>
         <PageTransition>
-          {children}
+        {children}
         </PageTransition>
+      
       </body>
     </html>
   );
