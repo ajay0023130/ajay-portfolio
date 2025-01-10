942fe31ca82663205a74a833cbee71850c5b00ef
AJAY BABU <42473536+ajay0023130@users.noreply.github.com>
2025-01-10 16:18:25 +0530

    Update Social.jsx

--- components/ui/Social.jsx
+++ components/ui/Social.jsx
@@ -1,31 +1,48 @@
-import Link from "next/link"
-import {FaGithub,FaLinkedin,FaYoutube,FaHackerrank,} from "react-icons/fa";
+import Link from "next/link";
+import { FaGithub, FaLinkedin, FaHackerrank } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
 const Socails = [
-    {
-        icon: <FaLinkedin />, path:"https://www.linkedin.com/in/ajay-babu-51740916a/"
-    },
-    {
-        icon: <FaGithub />, path:"https://github.com/ajay0023130"
-    },
-    {
-        icon: <FaHackerrank />, path:"https://www.hackerrank.com/profile/ajaybabu0046"
-    },
-    {
-        icon: <SiLeetcode />, path:"https://leetcode.com/u/ajaybabu0046/"
-    },
-]
+  {
+    icon: <FaLinkedin />,
+    path: "https://www.linkedin.com/in/ajay-babu-51740916a/",
+    label: "LinkedIn profile of Ajay Babu",
+  },
+  {
+    icon: <FaGithub />,
+    path: "https://github.com/ajay0023130",
+    label: "GitHub profile of Ajay Babu",
+  },
+  {
+    icon: <FaHackerrank />,
+    path: "https://www.hackerrank.com/profile/ajaybabu0046",
+    label: "HackerRank profile of Ajay Babu",
+  },
+  {
+    icon: <SiLeetcode />,
+    path: "https://leetcode.com/u/ajaybabu0046/",
+    label: "LeetCode profile of Ajay Babu",
+  },
+];
 
-const Social = ({containerStyles,iconStyles}) => {
+const Social = ({ containerStyles, iconStyles }) => {
   return (
     <div className={containerStyles}>
-        {Socails.map((item,index)=>{
-            return <Link key={index} href={item.path} className={iconStyles}>{item.icon}</Link>
-        })}
-   
+      {Socails.map((item, index) => (
+        <Link
+          key={index}
+          href={item.path}
+          aria-label={item.label}
+          className={iconStyles}
+          target="_blank" // Optional: Open in a new tab
+          rel="noopener noreferrer" // Recommended for security
+        >
+          {item.icon}
+        </Link>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Social
+export default Social;
+
